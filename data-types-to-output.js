1086d3b345c3aa45c09eab38e40e131ce64440e3
Beregszászi István <beistvan@gmail.com>
2024-03-09 11:18:41 +0100

    Data Types To Output

---
+++ data-types-to-output.js
@@ -0,0 +1,12 @@
+console.log('JavaScript');
+console.log(2011);
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+// Number
+// BigInt
+// String
+// Boolean
+// Null
+// Undefined
+// Symbol
+// Object
