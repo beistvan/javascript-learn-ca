49c23fb14d7f06fc3247b24c7f237e7ed4c6d728
Beregszászi István <beistvan@gmail.com>
2024-03-09 11:23:55 +0100

    String Methods

---
+++ string-methods.js
@@ -0,0 +1,10 @@
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy');
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   '.trim());
+console.log('Codecademy'.toUpperCase());
+
+
+console.log('hello'.toUpperCase()); // Prints 'HELLO'
+console.log('Hey'.startsWith('H')); // Prints true
