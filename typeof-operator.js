f27fc8644539bf453c36b9216bc567b2262360ee
Beregszászi István <beistvan@gmail.com>
2024-03-10 13:05:20 +0100

    Typeof Operator

---
+++ typeof-operator.js
@@ -0,0 +1,13 @@
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
+
+const unknown1 = 'foo';
+console.log(typeof unknown1); // Output: string
+
+const unknown2 = 10;
+console.log(typeof unknown2); // Output: number
+
+const unknown3 = true;
+console.log(typeof unknown3); // Output: boolean
