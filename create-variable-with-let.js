1c28956082dc467e780ed83f3c2982dd738e1bce
Beregszászi István <beistvan@gmail.com>
2024-03-10 12:51:00 +0100

    Create Variable With Let

---
+++ create-variable-with-let.js
@@ -0,0 +1,12 @@
+let changeMe = false;
+console.log(changeMe);
+
+let meal = 'Enchiladas';
+console.log(meal); // Output: Enchiladas
+meal = 'Burrito';
+console.log(meal); // Output: Burrito
+
+let price;
+console.log(price); // Output: undefined
+price = 350;
+console.log(price); // Output: 350
