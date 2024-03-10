96d39ebdbbb10f4c7f7c61bb8c81aadcbcc3c3e2
Beregszászi István <beistvan@gmail.com>
2024-03-10 12:53:05 +0100

    Create Variable With Const

---
+++ create-variable-with-const.js
@@ -0,0 +1,6 @@
+const entree = 'Enchiladas';
+console.log(entree);
+//entree = 'Tacos' // TypeError: Assignment to constant variable.
+
+const myName = 'Gilberto';
+console.log(myName); // Output: Gilberto
