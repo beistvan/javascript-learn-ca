23d463d59dc295355c1855dac564cdbd274508c8
Beregszászi István <beistvan@gmail.com>
2024-03-10 12:58:31 +0100

    Built-In Mathematical Assignment Operators

---
+++ built-in-mathematical-assignment-operators.js
@@ -0,0 +1,16 @@
+let w = 4;
+w += 1; // Can be written as w = w + 1
+
+console.log(w); // Output: 5
+
+let x = 20;
+x -= 5; // Can be written as x = x - 5
+console.log(x); // Output: 15
+
+let y = 50;
+y *= 2; // Can be written as y = y * 2
+console.log(y); // Output: 100
+
+let z = 8;
+z /= 2; // Can be written as z = z / 2
+console.log(z); // Output: 4
