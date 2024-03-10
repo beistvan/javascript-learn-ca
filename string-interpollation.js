5af3e85da784df1f86e3c0840e07011b0da7f89f
Beregszászi István <beistvan@gmail.com>
2024-03-10 13:04:27 +0100

    String Interpollation

---
+++ string-interpollation.js
@@ -0,0 +1,7 @@
+const myName = 'Jason';
+const myCity = 'New York';
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`);
+
+const myPet = 'armadillo';
+console.log(`I own a pet ${myPet}.`);
+// Output: I own a pet armadillo.
