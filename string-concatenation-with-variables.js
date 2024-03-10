4297593c786c08b1de9cebf1ac38b0afb6d3eb50
Beregszászi István <beistvan@gmail.com>
2024-03-10 13:01:49 +0100

    String Concatenation With Variables

---
+++ string-concatenation-with-variables.js
@@ -0,0 +1,6 @@
+let favoriteAnimal = "puma"; 
+console.log('My favorite animal: ' + favoriteAnimal);
+
+let myPet = 'armadillo';
+console.log('I own a pet ' + myPet + '.');
+// Output: 'I own a pet armadillo.'
