64bbb6dd805fc54abf0e2509c689e47818a86003
Beregszászi István <beistvan@gmail.com>
2024-03-10 13:30:20 +0100

    Dog Years

---
+++ dog-years.js
@@ -0,0 +1,11 @@
+//Your age as a number
+const myAge = 25;
+//The first two years of a dog’s life
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+//console.log(earlyYears, laterYears);
+let myAgeInDogYears = earlyYears + laterYears;
+const myName = "Istvan".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
