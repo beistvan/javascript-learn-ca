6603654f665d7e4327542857ae09640443e488e4
Beregszászi István <beistvan@gmail.com>
2024-03-11 09:40:20 +0100

    If...Else Statements

---
+++ if-else-statements.js
@@ -0,0 +1,17 @@
+let sale = true;
+
+sale = false;
+
+if (sale) {
+ console.log('Time to buy!');
+} else {
+  console.log('Time to wait for a sale.');
+}
+
+// if (false) {
+//  console.log('The code in this block will not run.');
+// } else {
+//  console.log('But the code in this block will!');
+// }
+
+// Prints: But the code in this block will!
