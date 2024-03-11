26e733a731b6bee3d8c3b169fd6c40e43e269284
Beregszászi István <beistvan@gmail.com>
2024-03-11 09:38:51 +0100

    If Statement

---
+++ if-statement.js
@@ -0,0 +1,9 @@
+let sale = true;
+sale = false;
+if (sale){
+  console.log("Time to buy!");
+}
+// if (true) {
+//  console.log('This message will print!'); 
+// }
+// Prints: This message will print!
