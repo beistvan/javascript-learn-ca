1028e69c9f88f55557f4d40805834ae6f75a1cd4
Beregszászi István <beistvan@gmail.com>
2024-03-12 10:50:45 +0100

    Logical Operators

---
+++ logical-operators.js
@@ -0,0 +1,23 @@
+let mood = 'sleepy';
+let tirednessLevel = 6;
+if (mood === 'sleepy' && tirednessLevel > 8) {
+  console.log('time to sleep');
+} else {
+  console.log('not bed time yet');
+}
+
+/*
+if (stopLight === 'green' && pedestrians === 0) {
+  console.log('Go!');
+} else {
+  console.log('Stop');
+}
+*/
+
+/*
+if (day === 'Saturday' || day === 'Sunday') {
+  console.log('Enjoy the weekend!');
+} else {
+  console.log('Do some work.');
+}
+*/
