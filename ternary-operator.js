ef142c62f7763f2921e7f266098fac80ea6dc800
Beregszászi István <beistvan@gmail.com>
2024-03-12 10:55:56 +0100

    Ternary Operator

---
+++ ternary-operator.js
@@ -0,0 +1,25 @@
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
+
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase === 'Love That!' ? console.log('I love that!') : console.log("I don't love that!");
+
+/*
+let isNightTime = true;
+
+if (isNightTime) {
+  console.log('Turn on the lights!');
+} else {
+  console.log('Turn off the lights!');
+}
+*/
+
+/*
+isNightTime ? console.log('Turn on the lights!') : console.log('Turn off the lights!'); 
+*/ 
