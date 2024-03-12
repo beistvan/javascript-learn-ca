2b2dd987bcaf7dedf624aae1072ac21f205f174c
Beregszászi István <beistvan@gmail.com>
2024-03-12 10:54:40 +0100

    Truthy And Falsy Assignment

---
+++ truthy-and-falsy-assignment.js
@@ -0,0 +1,26 @@
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+
+/*
+let username = '';
+let defaultName;
+
+if (username) {
+  defaultName = username;
+} else {
+  defaultName = 'Stranger';
+}
+
+console.log(defaultName); // Prints: Stranger
+ */
+
+/*
+let username = '';
+let defaultName = username || 'Stranger';
+
+console.log(defaultName); // Prints: Stranger
+*/
