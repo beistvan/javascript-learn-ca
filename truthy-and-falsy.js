ef358154f6aa5da897ae18045a9d11a083b34a6f
Beregszászi István <beistvan@gmail.com>
2024-03-12 10:52:55 +0100

    Truthy And Falsy

---
+++ truthy-and-falsy.js
@@ -0,0 +1,38 @@
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+
+/*
+let myVariable = 'I Exist!';
+
+if (myVariable) {
+   console.log(myVariable)
+} else {
+   console.log('The variable does not exist.')
+}
+*/
+
+/*
+let numberOfApples = 0;
+
+if (numberOfApples){
+   console.log('Let us eat apples!');
+} else {
+   console.log('No apples left!');
+}
+
+// Prints 'No apples left!'
+*/
