1ced6deae415f565cac3620f6d06e14d0a7f0700
Beregszászi István <beistvan@gmail.com>
2024-03-14 10:01:27 +0100

    Function Declarations

---
+++ function-declarations.js
@@ -0,0 +1,15 @@
+function getReminder() {
+  console.log('Water the plants.');
+}
+
+function greetInSpanish() {
+  console.log('Buenas tardes.');
+}
+
+/*
+greetWorld(); // Output: Hello, World!
+
+function greetWorld() {
+  console.log('Hello, World!');
+}
+*/
