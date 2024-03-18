7751bf2e1199862e86dc3f23aa71f280c0ed60f4
Beregszászi István <beistvan@gmail.com>
2024-03-18 10:16:52 +0100

    Global Scope

---
+++ global-scope.js
@@ -0,0 +1,5 @@
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+const stars = 'North Star';
+const callMyNightSky = () => 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+console.log(callMyNightSky());
