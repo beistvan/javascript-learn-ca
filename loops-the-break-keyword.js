f4d2e98d10b55e1610cfbf545fa4c5f6a6c33517
beistvan <beistvan@gmail.com>
2024-11-23 15:24:17 +0100

    Loops - The Break Keyword

---
+++ loops-the-break-keyword.js
@@ -0,0 +1,10 @@
+const rapperArray = ["Lil' Kim", "Jay-Z", "Notorious B.I.G.", "Tupac"];
+
+for (let i = 0; i < rapperArray.length; i++) {
+  if (rapperArray[i] == 'Notorious B.I.G.') {
+    console.log(rapperArray[i]);
+    break;
+  }  
+}
+
+console.log("And if you don't know, now you know.");
